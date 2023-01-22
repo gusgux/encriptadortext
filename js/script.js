43d8a4a55fd07429f26af5aeb2b5b5279e30efe2
gusgux <en_ge_l@hotmail.com>
2023-01-22 14:29:48 -0600

    encode function finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 
 
-function getText() {
+function encodeTex() {
 
     let text2Encode = document.getElementById("input-text").value;
     let text2Decode = document.getElementById("output-text");
-    let txInput = [];
     let btnCopy = document.getElementById("btn-copy");
     let btn2Encode = document.getElementById("btn-encode");
-    let btn2Decode = document.getElementById("btn-Decode");
-
+    let txtInput = [];
+    let txtEncode =[];
     const vowelCode = new Map();
     vowelCode.set("a","ai");
     vowelCode.set("e","enter");
@@ -17,33 +16,28 @@ function getText() {
     vowelCode.set("u","ufat");
     const code = {a:"ai", e:"enter", i:"imes", o:"ober" ,u:"ufat"};
 
-    //`"gato" => "gaitober"`
     btnCopy.style.display="flex";
-    //text2Decode.value+=text2Encode;
     text2Encode=text2Encode.trim().split(/\s+/g);
-    text2Encode.forEach(text => txInput.push(text.split("")));
-    let txtEncode=[];
-    for (let i = 0; i < txInput.length; i++) {
+    text2Encode.forEach(value => txtInput.push(value.split("")));
+   
+    text2Decode.value="";    for (const txt of txtInput) {
+        for (let i = 0; i < txt.length; i++) {
 
-        if(txInput[i]=="e") {
+            if(txt[i]=='a' || txt[i]=='e' || txt[i]=='i' || txt[i]=='o' || txt[i]=='u') {
 
-            txtEncode.push(vowelCode.get(txInput[i]));
-            //console.log("if ", txInput[i]);
+                txtEncode.push(vowelCode.get(txt[i]));
+            }
+            else {
 
-        }
-        else {
-            txtEncode.push(txInput[i]);
-            //console.log("else "+vowelCode.get(txInput[i]));
-        }       
-        
-    
-    }    
-    //console.log(txInput);
-    console.log(txtEncode);
-    text2Decode.value+=txtEncode;
+                txtEncode.push(txt[i]);
 
-    //text2Encode.forEach(element => console.log(element));
-    //code.forEach(element => console.log(element));
+            }       
+        }  
+        txtEncode.push(" ");
+    }
+    text2Decode.value+=txtEncode.join("");
+
+
 
     //vowelCode.forEach((value, key, map) => {console.log(key,value);} );
     
@@ -51,9 +45,13 @@ function getText() {
     
 }
 
+function decodeTex(){
+
+}
+
 function isValid(text) {
     return  text.length>1 ? !/[^a-z\sñ]/.test(text): "ok";
 }
 let encodeText = document.getElementById("btn-encode");
- encodeText.onclick = getText;
+ encodeText.onclick = encodeTex;
  
